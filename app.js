a9e67dff9d35289c7fd99c3b2e1a9c3da66f32e5
AntoineBlanot <antoineblanot@gmail.com>
2022-01-04 17:24:40 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,40 @@
+'use strict';
+
+const Readline = require('readline'); // for including readline module in app
+const rl = Readline.createInterface({ // for reading inputs
+    input : process.stdin,
+    output : process.stdout,
+    terminal : false
+});
+
+const matcher = require('./matcher'); // matcher module
+const weather = require('./weather'); // weather module
+
+rl.setPrompt('> ');
+rl.prompt();
+rl.on('line', reply => {
+    matcher(reply , cb => {
+        switch(cb.intent){
+
+            case 'Hello':
+                console.log(`${cb.entities.greeting} there`);
+                break;
+
+            case 'Exit':
+                console.log('You want to exit');
+                process.exit();
+
+            case 'get weather':
+                console.log(`Weather in ${cb.entities.city} at ${cb.entities.time} is :`);
+                break;
+
+            case 'current weather':
+                console.log(`Current weather in ${cb.entities.city} : `);
+                break;
+    
+            default:
+                console.log('I did not understand your resquest, please try again');
+        }
+    });
+    rl.prompt();
+});
