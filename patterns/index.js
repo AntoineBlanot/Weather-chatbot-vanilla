3d493d7479e46cd05e62d5eedd17493f1259275a
AntoineBlanot <antoineblanot@gmail.com>
2022-01-13 19:41:53 +0100

    add is it xxx in xxx? in forecast and current

--- patterns/index.js
+++ patterns/index.js
@@ -5,10 +5,10 @@ const patternDict = [{
         pattern :'\\b(bye|exit)\\b',
         intent : 'Exit'
     }, {
-        pattern : '\\b(weather)\\s(like\\s)?in\\s\\b(?<city>[A-Za-z]+\\s?([A-Za-z]+)?)\\s\\b(?<time>tomorrow|today)',
+        pattern : '\\b((weather\\s(like\\s)?)|(is\\sit\\s[a-z]+\\s))in\\s\\b(?<city>[A-Za-z]+\\s?([A-Za-z]+)?)\\s\\b(?<time>tomorrow|today)',
         intent : 'forecast weather'
     }, {
-        pattern : '\\b(weather)\\s(like\\s)?in\\s\\b(?<city>[A-Za-z]+\\s?([A-Za-z]+)?)',
+        pattern : '\\b((weather\\s(like\\s)?)|(is\\sit\\s[a-z]+\\s))in\\s\\b(?<city>[A-Za-z]+\\s?([A-Za-z]+)?)',
         intent : 'current weather'
     }];
     
